5394133a21cddb21b41c9d7296ee83112773c64b
butaixianran <butaixianran@qq.com>
2023-09-28 09:50:28 +0800

    fix nsfw issue

--- scripts/ch_lib/civitai.py
+++ scripts/ch_lib/civitai.py
@@ -345,10 +345,9 @@ def get_preview_image_by_model_path(model_path:str, max_size_preview, skip_nsfw_
                 if model_info["images"]:
                     for img_dict in model_info["images"]:
                         if "nsfw" in img_dict.keys():
-                            if img_dict["nsfw"] != "None": 
-                                util.printD("This image is NSFW: " + str(img_dict["nsfw"]))
-                                current_nsfw = img_dict.get("nsfw", "None")
-                                if should_skip(skip_nsfw_preview, current_nsfw):
+                            if img_dict["nsfw"] and img_dict["nsfw"] != "None":
+                                util.printD("This image is NSFW")
+                                if skip_nsfw_preview:
                                     util.printD("Skip NSFW image")
                                     continue
                         
